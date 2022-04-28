63eec6b2d91cfa1bc3132c2a0865d9388b07ac47
Riser17 <harshmaharajpura1998@gmail.com>
2022-04-28 11:23:52 +0530

    web updated

--- src/component/UploadNFT.jsx
+++ src/component/UploadNFT.jsx
@@ -1,20 +1,21 @@
 import React, {useState, useEffect} from 'react';
 // import './App.css';
 import FileUploadScreen from '../screens/FileUploadScreen';
-import {getSingleFiles, getMultipleFiles} from '../data/api';
+// import {getSingleFiles, getMultipleFiles} from '../data/api';
+import {getMultipleFiles} from '../data/api';
 
 function UploadNFT() {
-  const [singleFiles, setSingleFiles] = useState([]);
+  // const [singleFiles, setSingleFiles] = useState([]);
   const [multipleFiles, setMultipleFiles] = useState([]);
 
-  const getSingleFileslist = async () => {
-    try {
-        const fileslist = await getSingleFiles();
-        setSingleFiles(fileslist);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // const getSingleFileslist = async () => {
+  //   try {
+  //       const fileslist = await getSingleFiles();
+  //       setSingleFiles(fileslist);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
   const getMultipleFilesList = async () => {
     try {
         const fileslist = await getMultipleFiles();
@@ -25,18 +26,19 @@ function UploadNFT() {
     }
   }
   useEffect(() => {
-    getSingleFileslist();
+    // getSingleFileslist();
     getMultipleFilesList();
   }, []);
   return (
     <>
         <div className="container">
-          <h3 className="text-danger font-weight-bolder border-bottom text-center">Single & Multiple File Upload Using MERN Stack </h3>
-          <FileUploadScreen getsingle={() => getSingleFileslist()} getMultiple={() => getMultipleFilesList()}/>
+          
+          <FileUploadScreen  getMultiple={() => getMultipleFilesList()}/>
+          {/* getsingle={() => getSingleFileslist()} */}
        </div> 
        <div className="container-fluid mt-5">
          <div className="row">
-           <div className="col-6">
+           {/* <div className="col-6">
              <h4 className="text-success font-weight-bold">Single Files List</h4>
              <div className="row">
                 {singleFiles.map((file, index) => 
@@ -46,8 +48,8 @@ function UploadNFT() {
                       </div>
                   </div>
                 )}
-             </div>
-           </div>
+             </div> */}
+           {/* </div> */}
            <div className="col-6">
              <h4 className="text-success font-weight-bold">Multiple Files List</h4>
              {multipleFiles.map((element, index) =>
--- src/screens/FileUploadScreen.js
+++ src/screens/FileUploadScreen.js
@@ -1,31 +1,34 @@
 import React, {useState, useEffect} from 'react';
-import {singleFileUpload, multipleFilesUpload} from '../data/api';
+// import {singleFileUpload, multipleFilesUpload} from '../data/api';
 // import {CircularProgressbar, buildStyles} from 'react-circular-progressbar';
 // import 'react-circular-progressbar/dist/styles.css';
-
+import {multipleFilesUpload} from '../data/api';
 
 const FileUploadScreen = (props) => {
-    const [singleFile, setSingleFile] = useState('');
+    // const [singleFile, setSingleFile] = useState('');
     const [multipleFiles, setMultipleFiles] = useState('');
     const [title, setTitle] =  useState('');
-    const [singleProgress, setSingleProgress] = useState(0);
+    const [NFTname, setNFTname] =  useState('');
+    const [AssetID, setAssetID] =  useState('');
+    const [Description, setDescription] =  useState('');
+    // const [singleProgress, setSingleProgress] = useState(0);
     const [multipleProgress, setMultipleProgress] = useState(0);
 
-    const SingleFileChange = (e) => {
-        setSingleFile(e.target.files[0]);
-        setSingleProgress(0);
-    }
+    // const SingleFileChange = (e) => {
+    //     setSingleFile(e.target.files[0]);
+    //     setSingleProgress(0);
+    // }
     const MultipleFileChange = (e) => {
         setMultipleFiles(e.target.files);
         setMultipleProgress(0);
     }
-    const singleFileOptions = {
-        onUploadProgress: (progressEvent) => {
-            const {loaded, total} = progressEvent;
-            const percentage = Math.floor(((loaded / 1000) * 100) / (total / 1000));
-            setSingleProgress(percentage);
-        }
-    }
+    // const singleFileOptions = {
+    //     onUploadProgress: (progressEvent) => {
+    //         const {loaded, total} = progressEvent;
+    //         const percentage = Math.floor(((loaded / 1000) * 100) / (total / 1000));
+    //         setSingleProgress(percentage);
+    //     }
+    // }
     const mulitpleFileOptions = {
         onUploadProgress: (progressEvent) => {
             const {loaded, total} = progressEvent;
@@ -33,15 +36,19 @@ const FileUploadScreen = (props) => {
             setMultipleProgress(percentage);
         }
     }
-    const uploadSingleFile = async () => {
-        const formData = new FormData();
-        formData.append('file', singleFile);
-        await singleFileUpload(formData, singleFileOptions);
-        props.getsingle();
-    }
+    // const uploadSingleFile = async () => {
+    //     const formData = new FormData();
+    //     formData.append('file', singleFile);
+    //     await singleFileUpload(formData, singleFileOptions);
+    //     props.getsingle();
+    // }
     const UploadMultipleFiles = async () => {
         const formData = new FormData();
         formData.append('title', title);
+        formData.append('NFTname', NFTname);
+        formData.append('AssetID', AssetID);
+        formData.append('Description', Description);
+        
         for (let i = 0; i < multipleFiles.length; i++) {
             formData.append('files', multipleFiles[i]);                      
         }
@@ -50,7 +57,7 @@ const FileUploadScreen = (props) => {
     }
     return (
         <div className="row mt-3">
-            <div className="col-6">
+            {/* <div className="col-6">
                 <div className="form-group">
                     <label>Select Single File</label>
                     <input type="file" className="form-control" onChange={(e) => SingleFileChange(e)} />
@@ -58,7 +65,7 @@ const FileUploadScreen = (props) => {
                 <div className="row">
                     <div className="col-10">
                         <button type="button" className="btn btn-danger" onClick={() => uploadSingleFile()} >Upload</button>
-                    </div>
+                    </div> */}
                     {/* <div className="col-2">
                         <CircularProgressbar
                             value={singleProgress}
@@ -75,13 +82,25 @@ const FileUploadScreen = (props) => {
                             })}
                         />
                     </div> */}
-                </div>
-            </div>
-            <div className="col-6">
+                {/* </div>
+            </div> */}
+            <div className="">
                    <div className="row">
                        <div className="col-6">
                             <label >Title</label>
                             <input type="text" onChange={(e) => setTitle(e.target.value) } placeholder="enter title for your gallery" className="form-control"/>
+                       </div>
+                       <div className="col-6">
+                            <label >NFTname</label>
+                            <input type="text" onChange={(e) => setNFTname(e.target.value) } placeholder="enter title for your gallery" className="form-control"/>
+                       </div>
+                       <div className="col-6">
+                            <label >AssetID</label>
+                            <input type="number" onChange={(e) => setAssetID(e.target.value) } placeholder="enter title for your gallery" className="form-control"/>
+                       </div>
+                       <div className="col-6">
+                            <label >Description</label>
+                            <input type="text" onChange={(e) => setDescription(e.target.value) } placeholder="enter title for your gallery" className="form-control"/>
                        </div>
                        <div className="col-6">
                         <div className="form-group">
